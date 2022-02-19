2156e4b703794b15dd7da0a914cffd8939763f12
Irshad <Irshadck35@gmail.com>
2022-02-19 20:05:54 +0530

    added

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.views.generic.base import RedirectView
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 app_name="main"
 
 urlpatterns=[
       path("",views.homepage,name="homepage"),
+      re_path(r'^favicon\.ico$', favicon_view),
 ]
